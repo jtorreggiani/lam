c55a7cec5b2285993e7acb1428ca712e49453380
jtorreggiani <joetorreggiani@gmail.com>
2025-02-01 23:10:59 -0500

    Use doc strings.

--- src/term.rs
+++ src/term.rs
@@ -1,14 +1,14 @@
-// Represents a term in our logic programming language.
+/// Represents a term in our logic programming language.
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub enum Term {
-    // A constant value (for example, a 32‐bit integer).
+    /// A constant value (for example, a 32‐bit integer).
     Const(i32),
-    // A variable, identified by a name.
+    /// A variable, identified by a name.
     Var(String),
-    // A compound term with a functor and a list of arguments.
+    /// A compound term with a functor and a list of arguments.
     Compound(String, Vec<Term>),
-    // A lambda abstraction: λ<param>.<body>
+    /// A lambda abstraction: λ<param>.<body>
     Lambda(String, Box<Term>),
-    // An application: (<function> <argument>)
+    /// An application: (<function> <argument>)
     App(Box<Term>, Box<Term>),
 }
